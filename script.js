3ecf704a5c96035120cd2dc17c90f25b5a045cfa
mserafin20 <mmserafin20@gmail.com>
2023-04-04 14:22:00 -0400

    added a for loop to pull random characters from the characterPool and create a password

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Possible password characters
 
-var uniqueChar = ["?",">","<","!","@","#","$","%","^","&","*"];
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uniqueChar = ["?",">","<","!","@","#","$","%","^","&","*"]
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var numbers = [0,1,2,3,4,5,6,7,8,9];
 
 // Generate Password
@@ -14,7 +14,7 @@ var finalArray = "";
 function generatePassword(){
   passwordLength = prompt("Select a password between 8 and 128 characters");
 
-  if (passwordLength >= 8 && passwordLength <= 128 && Number.isInterger(Number(passwordLength))){
+  if (passwordLength >= 8 && passwordLength <= 128 && Number.isInterger(Number(passwordLength))) {
 
 // Confirmation box that includes uppercase letters
 
@@ -24,16 +24,39 @@ function generatePassword(){
     }
 // Confirmation box for lowercase letters
 
-    var lowerCase = confirm("Include Lower case?")
-    if (lowerCase) {
+    var useLowerCase = confirm("Include Lower case?")
+    if (useLowerCase) {
       characterPool = characterPool.concat(lowerCase)
     }
 
-    
+  // Confirmation box for unique characters 
+
+    var useUniqueChar = confirm("Include a unique character?")
+    if (useUniqueChar) {
+      characterPool = characterPool.concat(uniqueChar)
+    }
+
+  // Confirmation box for numbers 
+
+  var useNumeric = confirm("Include numbers?")
+  if (useNumeric) {
+    characterPool = characterPool.concat(numbers)
+    }
+
+  // Pulls random characters from characterPool to create password
+
+  for (var i = 0; i < parseInt(passwordLength); i++){
+    var randomIndex = Math.floor(Math.random() * characterPool.length);
+    var x = characterPool[randomIndex]
+    finalArray = finalArray.concat(x);
+
+    }
 
   }
+
 }
 
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
